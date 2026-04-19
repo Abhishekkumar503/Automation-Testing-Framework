c7548ade22a1f82e3b6b397e8b2babefe8d36e81
Abhishekkumar503 <ak204479@gmail.com>
2026-04-20 02:28:26 +0530

    Add one testscript with Abstraction method

--- src/test/java/com/ui/test/LoginTest.java
+++ src/test/java/com/ui/test/LoginTest.java
@@ -5,15 +5,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.utility.BrowserUtility;
+
 public class LoginTest {
 
 	public static void main(String[] args) {
 		
 		WebDriver driver = new ChromeDriver(); // Loose Coupling ( we can change frequently to another browser whene ever needed)
-		driver.manage().window().maximize();
-		driver.get("https://automationpractice.techwithjatin.com/my-account");
-		driver.findElement(By.className("login")).click();
-		driver.findElement(By.id("email")).sendKeys("senax57658@sixoplus.com");
+		BrowserUtility browserUtility = new BrowserUtility(driver);
+		browserUtility.goToWebsite("https://automationpractice.techwithjatin.com/my-account");
+		browserUtility.maximizeWindow();
+		
+		browserUtility.clickOn(By.className("login"));
+		browserUtility.enterText(By.id("email"),"senax57658@sixoplus.com");
 		driver.findElement(By.id("passwd")).sendKeys("0987654321");
 		
 		// Another way
